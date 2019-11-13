255aeda9f6c8f1e5aee38b57d4d3e7c3bc8cc38d
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-11-13 10:15:50 -0700

    finished stage one

--- react-hello-world/src/state-drills/HelloWorld.js
+++ react-hello-world/src/state-drills/HelloWorld.js
@@ -1,7 +1,26 @@
 import React from "react";
 
-function HelloWorld() {
-  return <div className="HelloWorld"></div>;
+class HelloWorld extends React.Component {
+  state = {
+    who: "Hello, world"
+  };
+  handleButtonClick = word => {
+    this.setState({
+      who: word
+    });
+  };
+  render() {
+    return (
+      <div>
+        <p>{this.state.who}</p>
+        <button onClick={() => this.handleButtonClick("world")}>world</button>
+        <button onClick={() => this.handleButtonClick("friend")}>friend</button>
+        <button onClick={() => this.handleButtonClick("react")}>react</button>
+      </div>
+    );
+  }
 }
 
 export default HelloWorld;
+
+//When a user clicks on one of the three buttons, use state and an event handler to update the state property who so that the content in the p element changes.
